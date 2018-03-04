655ca032023680d613ba8e90a976673f23f41f78
Benjamen King <brk.3rdof3kings@gmail.com>
2018-03-04 09:16:13 -0700

    User can play songs in iTunes results list

--- www/src/components/SearchResults.vue
+++ www/src/components/SearchResults.vue
@@ -9,12 +9,17 @@
     </div>
 
     <div class="song p-1 row px-4 py-2 align-items-center" v-for="song in songs">
-      <span class="play-btn col-1"><i class="fa fa-play-circle" aria-hidden="true"></i></span>
+      <button class="play-btn btn btn-sm btn-outline-light col-1" v-show="showPlayBtn(song)" @click="play(song.trackId, song.previewUrl)">
+        <i class="fa fa-play-circle"></i>
+      </button>
+      <button class="play-btn btn btn-sm btn-outline-light col-1" v-show="showPauseBtn(song)" @click="pause(song.trackId, song.previewUrl)">
+        <i class="fas fa-pause-circle"></i>
+      </button>
       <span class="title col-4">{{song.trackName}}</span>
       <span class="album col-4">{{song.collectionName}}</span>
       <span class="time col-1">{{getMinsFromMS(song.trackTimeMillis)}}</span>
       <span class="time col-2">
-        <button class="btn btn-sm btn-outline-dark" @click="submit(song)">Add to My Music</button>
+        <button class="btn btn-sm btn-outline-light" @click="submit(song)">Add to My Music</button>
       </span>
     </div>
     
@@ -33,12 +38,20 @@
     data () {
       return {
         displayAddForm: false,
-        song: {}
+        selectedSong: {
+          id: "",
+          audio: {ended: true},
+          // timeout: ""
+        },
+        // audioIsPlaying: false
       }
     },
     computed: {
       songs() {
         return this.$store.state.iTunesResults
+      },
+      audioIsPlaying() {
+        return !(this.selectedSong.audio.ended || this.selectedSong.audio.paused)
       }
     },
     methods: {
@@ -68,6 +81,35 @@
       },
       hideAddSongForm() {
         this.displayAddForm = false
+      },
+      play(songId, audioSrc) {
+        if (this.audioIsPlaying) {
+          this.selectedSong.audio.pause()
+          // console.log('timeoutId:', this.selectedSong.timeout)
+          // clearTimeout(this.selectedSong.timeout)
+        }
+        this.selectedSong.id = songId
+        this.selectedSong.audio = new Audio(audioSrc)
+        this.selectedSong.audio.play()
+        this.audioIsPlaying = true
+        // this.selectedSong.timout = setTimeout(() => {
+        //   this.audioIsPlaying = false
+        //   this.selectedSong.id = ""
+        // }, 30000)
+      },
+      pause(songId, audioSrc) {
+        this.selectedSong.audio.pause()
+        // console.log('timeoutId:', this.selectedSong.timeout)
+        // clearTimeout(this.selectedSong.timeout)
+        this.selectedSong.id = ""
+        this.audioIsPlaying = false
+      },
+      showPlayBtn(song) {
+        return this.selectedSong.id != song.trackId
+      },
+      showPauseBtn(song) {
+        // return this.selectedSong.id == song.trackId && this.audioIsPlaying
+        return this.selectedSong.id == song.trackId && !(this.selectedSong.audio.ended || this.selectedSong.audio.paused)
       }
     }
   }
@@ -78,7 +120,8 @@
     background-color: #f8f9fa;
   }
   .song:hover {
-    background-color: #8fb6cb;
+    /* background-color: #8fb6cb; */
+    background-color: #17a2b8;
   }
   .song:hover span {
     color: #fff;
