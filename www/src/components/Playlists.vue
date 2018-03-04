8c54d75a37b6b22ad762b24a7d9553ab87b876ab
Benjamen King <brk.3rdof3kings@gmail.com>
2018-03-04 16:23:24 -0700

    User can click on the Playlists.vue sidebar to load different playlists in Home.vue

--- www/src/components/Playlists.vue
+++ www/src/components/Playlists.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="playlists">
     <h3 class="h4 text-center pb-3 border-bottom">Playlists</h3>
-    <div class="playlist d-flex flex-column align-items-center mt-4" v-for="playlist in playlists">
-      <img :src="playlist.imgUrl" alt="playlist image" class="w-75 rounded-circle mb-4">
-      <h5 class="text-center mb-4">{{playlist.title}}</h5>
-      <p class="desc">{{playlist.desc}}</p>
+    <div class="playlist mt-4" v-for="playlist in playlists">
+      <a href="#" class="text-info" @click.prevent="setActivePlaylist(playlist)">
+        <div class="d-flex flex-column align-items-center">
+          <img :src="playlist.imgUrl" alt="playlist image" class="w-75 rounded-circle mb-4">
+          <h5 class="text-center mb-4">{{playlist.title}}</h5>
+          <p class="desc">{{playlist.desc}}</p>
+        </div>
+      </a>
     </div>
   </div>
 </template>
@@ -26,7 +30,9 @@ export default {
     }
   },
   methods: {
-
+    setActivePlaylist(playlist) {
+      this.$store.dispatch('getPlaylist', playlist._id)
+    }
   }
 }
 </script>
